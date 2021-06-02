d982b5375f9de047b5d909422bd3989fef2088cd
Conrado Quilles Gomes <conradoqg@gmail.com>
2021-06-01 23:25:02 -0300

    Improve dotfiles structure

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 ```console
 $ sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply conradoqg
 ```
+
+## Testing
+
+```console
+$ docker run -it --rm -v $PWD:/root/.local/share/chezmoi ubuntu-chezmoi bash
+$ sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply
+```
--- run_once_01-oh-my-zsh.sh
+++ run_once_01-oh-my-zsh.sh
@@ -2,14 +2,17 @@
 
 echo "Clonning or updating oh-my-zsh"
 
-git -C ~/.oh-my-zsh pull || git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git -C ~/.oh-my-zsh pull || git clone --depth 1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh 
 
 echo "Clonning or updating plugins"
 
-git -C ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions pull || git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git -C ~/.oh-my-zsh-custom/plugins/zsh-autosuggestions pull || git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh-custom/plugins/zsh-autosuggestions
 
-git -C ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting pull || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git -C ~/.oh-my-zsh-custom/plugins/zsh-syntax-highlighting pull || git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh-custom/plugins/zsh-syntax-highlighting
 
 echo "Clonning or updating theme"
 
-git -C ~/.oh-my-zsh/custom/themes/powerlevel10k pull || git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+git -C ~/.oh-my-zsh-custom/themes/powerlevel10k pull || git clone --depth 1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh-custom/themes/powerlevel10k
+
+echo "Making ZSH the default shell"
+chsh -s $(which zsh)
