290932ee2066b091b6c66daa3d3b3a018a4658ea
Conrado Quilles Gomes <conradoqg@gmail.com>
2021-06-01 22:09:06 -0300

    Add theme clonning to run_once_01-oh-my-zsh.sh

--- run_once_01-oh-my-zsh.sh
+++ run_once_01-oh-my-zsh.sh
@@ -9,3 +9,7 @@ echo "Clonning or updating plugins"
 git -C ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions pull || git clone zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 git -C ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting pull || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+echo "Clonning or updating theme"
+
+git -C ~/.oh-my-zsh/custom/themes/powerlevel10k pull || git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
